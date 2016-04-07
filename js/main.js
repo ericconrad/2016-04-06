f3964fd793130f714846da8507f0c6d7b0555931
Eric Conrad <eric.conrad@jhu.edu>
2016-04-06 22:18:43 -0400

    bleep blorp

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $(document).ready( function() {
-  alert("STILL WORKING");
+
+  $("#meow").click( function() {
+    $("#animals").append("<img class='animal' src='images/nyancat.png' alt='Nyan Cat' />");
+  });
+
+  $(".animal").click( function() {
+    alert(blah);
+    // $(this).css('visibility', 'hidden');
+  });
+
 });
